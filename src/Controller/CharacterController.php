b9afb0c2a4cb32e6638fa2d10ecc5bd7df23e14d
root <root@server.sebastian-christoph.de>
2020-02-26 16:13:58 +0100

    asd

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 
 use App\Entity\Character;
 use App\Entity\User;
+use App\Form\CharacterType;
 use App\Repository\CharacterRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,6 +31,23 @@ class CharacterController extends AbstractController
     public function characters(): Response
     {
         return $this->render('characters/index.html.twig');
+    }
+
+    /**
+     * @Route("/characters/create", name="create", methods={"GET","POST"})
+     */
+    public function create(Request $request): Response
+    {
+        $form = $this->createForm(CharacterType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $this->addFlash('success', 'Char erstellt.');
+        }
+
+        return $this->render('characters/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     public function listCharacters(): Response
---
+++ src/Form/CharacterType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Character;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CharacterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('charname')
+            //->add('user')
+            ->add('create', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Character::class,
+        ]);
+    }
+}
