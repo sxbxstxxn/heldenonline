fb55392bdcd26aa37440adb884b4753098338693
root <root@server.sebastian-christoph.de>
2020-04-13 14:26:02 +0200

    added example for advantag choser

--- public/js/base.js
+++ public/js/base.js
@@ -1,5 +1,43 @@
 /*
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    //$('[data-toggle="tooltip"]').tooltip()
+
 });
 */
+
+$(function () {
+
+    $('.form-control-chosen').chosen({
+        allow_single_deselect: true,
+        width: '100%'
+    });
+    /*
+    $('.form-control-chosen-required').chosen({
+        allow_single_deselect: false,
+        width: '100%'
+    });
+    $('.form-control-chosen-search-threshold-100').chosen({
+        allow_single_deselect: true,
+        disable_search_threshold: 100,
+        width: '100%'
+    });
+    $('.form-control-chosen-optgroup').chosen({
+        width: '100%'
+    });
+
+    $(function() {
+        $('[title="clickable_optgroup"]').addClass('chosen-container-optgroup-clickable');
+    });
+    $(document).on('click', '[title="clickable_optgroup"] .group-result', function() {
+        var unselected = $(this).nextUntil('.group-result').not('.result-selected');
+        if(unselected.length) {
+            unselected.trigger('mouseup');
+        } else {
+            $(this).nextUntil('.group-result').each(function() {
+                $('a.search-choice-close[data-option-array-index="' + $(this).data('option-array-index') + '"]').trigger('click');
+            });
+        }
+    });
+
+     */
+});
